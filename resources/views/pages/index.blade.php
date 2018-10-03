06f10e9be8be4d8c4cb3d918242da3eca999e99d
MasterDuwi <dyllnamoc@gmail.com>
2018-10-03 18:05:04 +0800

    Made some assets fetched using secure_asset.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,7 +12,7 @@
      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
  
      <!-- Custom fonts for this template -->
-     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+     <link href="{{ secure_asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
      <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
      <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
  
@@ -156,13 +156,13 @@
               </section>
           
               <!-- Bootstrap core JavaScript -->
-              <script src="vendor/jquery/jquery.min.js"></script>
-              <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+              <script src="{{ secure_asset('vendor/jquery/jquery.min.js') }}"></script>
+              <script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
           
               <!-- Plugin JavaScript -->
-              <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-              <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
-              <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+              <script src="{{ secure_asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+              <script src="{{ secure_asset('vendor/scrollreveal/scrollreveal.min.js') }}"></script>
+              <script src="{{ secure_asset('vendor/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
           
               <!-- Custom scripts for this template -->
               <script src="{{secure_asset('js/creative.min.js')}}"></script>
