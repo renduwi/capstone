e50c2e6c8df86efec0511ca05b1c5ad287db58a5
MasterDuwi <dyllnamoc@gmail.com>
2018-10-03 18:31:37 +0800

    commented

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,12 +12,12 @@
      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
  
      <!-- Custom fonts for this template -->
-     <link href="{{ secure_asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+     {{-- <link href="{{ secure_asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css"> --}}
+     {{-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
      <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
- 
+  --}}
      <!-- Plugin CSS -->
-     <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+     {{-- <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet"> --}}
  
      <!-- Custom styles for this template -->
      <link href="{{secure_asset('css/creative.min.css') }}" rel="stylesheet">
@@ -156,14 +156,14 @@
               </section>
           
               <!-- Bootstrap core JavaScript -->
-              <script src="{{ secure_asset('vendor/jquery/jquery.min.js') }}"></script>
+              {{-- <script src="{{ secure_asset('vendor/jquery/jquery.min.js') }}"></script>
               <script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
           
               <!-- Plugin JavaScript -->
               <script src="{{ secure_asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
               <script src="{{ secure_asset('vendor/scrollreveal/scrollreveal.min.js') }}"></script>
               <script src="{{ secure_asset('vendor/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
-          
+           --}}
               <!-- Custom scripts for this template -->
               <script src="{{secure_asset('js/creative.min.js')}}"></script>
           
