f3d7d274270eec29bb3d9d018af9683fadb0578c
MasterDuwi <dyllnamoc@gmail.com>
2018-10-04 16:58:44 +0800

    bb

--- app/Http/Controllers/ServiceReqController.php
+++ app/Http/Controllers/ServiceReqController.php
@@ -128,9 +128,9 @@ class ServiceReqController extends Controller
      */
     public function show($Sid)
     {
-       // $user_id = auth()->user()->Mid;
-        // $user = ServiceReq::find($user_id);
-        // return view('servicereq.show')->with('serv', $serv);
+        $user_id = auth()->user()->Mid;
+         $user = ServiceReq::find($user_id);
+        return view('servicereq.show')->with('serv', $serv);
         //return view('servicereq.show');
     }
 
