2c8bf803de41d4fbe57293222aa72839515cfb5a
MasterDuwi <dyllnamoc@gmail.com>
2018-10-04 09:59:14 +0800

    dd2

--- resources/views/Admin_repairshop/index.blade.php
+++ resources/views/Admin_repairshop/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app(repairshop)')
 @section('content')
 <div class="container">
         <a class="btn btn-success btn-lg" href="/repair/create" role="button">Create</a>
