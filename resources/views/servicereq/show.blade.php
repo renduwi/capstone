740a59dbde889416137d39a726e28def8e70fda9
MasterDuwi <dyllnamoc@gmail.com>
2018-10-04 12:06:08 +0800

    dd9

--- resources/views/servicereq/show.blade.php
+++ resources/views/servicereq/show.blade.php
@@ -2,26 +2,33 @@
 
 @section('content')
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<div class="container">
+        <div class="row justify-content-center">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+    @if(count($serv) > 0)
+    <div class="table-responsive{-sm|-md|-lg|-xl}">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th class="text-center">#</th>
+                <th>Situation</th>
+                <th>Date</th>
+                <th>Vehicle</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+                @foreach($serv as $ser)
+                <tr>
+                    <td>{{$ser->situation}}</td>
+                    <td>{{$ser->created_at}}</td>
+                    <td>{{$ser->vehicle}}</td>
+                    <td>{{$ser->status}}</td>
+                </tr>
+                @endforeach
 
-<a href="/vehicle/show" class="btn btn-danger">
-    <i class="material-icons md-18">add_alert</i>
-</a>
-
-{{-- @if(count($Serv) > 0)
-<div class="table-responsive{-sm|-md|-lg|-xl}">
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th class="text-center">ID</th>
-            <th>Mechanic Name</th>
-            <th>Situation</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($vehicle as $veh)
-        <tr>
-            <td>{{$veh->Veid}}</td>
-            <td>{{$veh->name}}</td>
-            <td>{{$veh->brand}}</td>
-            <td>{{$veh->model}}</td> --}}
 @endsection
